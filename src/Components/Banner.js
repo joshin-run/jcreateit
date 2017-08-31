5283ce8ee1af3799366b2584a8b95d5f697a31c2
joshin-run <jahmsc@gmail.com>
2017-08-30 21:09:56 -0600

    Feat (style) set up components, set up sass/mapping, jh-logo.ico change but did not work, created navbar and that worked.

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar'
 
-class App extends Component {
+class Banner extends Component {
   render() {
     return (
       <div className="hero-banner">
-
+        <Navbar />
       </div>
     );
   }
 }
 
-export default App;
+export default Banner;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="">
+        <p>Navbar1000</p>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
