14db76e009dd3bcfd108d8ff42d4b247fa74885a
joshin-run <jahmsc@gmail.com>
2017-09-01 12:58:04 -0600

    Feat (style) projects and bio

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,18 +7,18 @@ import projFlowState from '../images/projects/FlowState.jpg'
 class Projects extends Component {
   render() {
     return (
-      <div id="projects-wrapper" className="projects-wrapper background-projects">
-        <div class="thumb-6">
-          <a href=" https://joshin-run.github.io/edison-tribute/" target="_blank"><img class="thumbnails image" src={projEdison} alt="Thomas Edison" /></a>
+      <div id="projects-wrapper" className="projects-wrapper">
+        <div className="thumb-6">
+          <a href=" https://joshin-run.github.io/edison-tribute/" target="_blank"><img className="thumbnails image" src={projEdison} alt="Thomas Edison" /></a>
         </div>
-        <div class="thumb-2">
-          <a href="https://joshin-run.github.io/white-vision/" target="_blank"><img class="thumbnails" src={projWhiteVision} alt="White Vision" /></a>
+        <div className="thumb-2">
+          <a href="https://joshin-run.github.io/white-vision/" target="_blank"><img className="thumbnails" src={projWhiteVision} alt="White Vision" /></a>
         </div>
-        <div class="thumb-4">
-          <a href="https://joshin-run.github.io/fcc-css-grid/" target="_blank"><img class="thumbnails" src={projCSSGrid} alt="CSS Grid Meetup Presentation" /></a>
+        <div className="thumb-4">
+          <a href="https://joshin-run.github.io/fcc-css-grid/" target="_blank"><img className="thumbnails" src={projCSSGrid} alt="CSS Grid Meetup Presentation" /></a>
         </div>
-        <div class="thumb-8">
-          <a href="https://joshin-run.github.io/flowstate-v" target="_blank"><img class="thumbnails" src={projFlowState} alt="FlowState" /></a>
+        <div className="thumb-8">
+          <a href="https://joshin-run.github.io/flowstate-v" target="_blank"><img className="thumbnails" src={projFlowState} alt="FlowState" /></a>
         </div>
       </div>
     );
