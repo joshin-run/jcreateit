9c74557699a20667c432f5f1e045788ec41d6d92
joshin-run <jahmsc@gmail.com>
2018-01-10 14:01:36 -0700

    Chore - take 'personal-projects' component out for now, unfinished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Banner from './Components/Banner';
 import WhatWeDo from './Components/WhatWeDo';
 import RecentWork from './Components/RecentWork';
 import Testimonials from './Components/Testimonials';
-import PersonalProjects from './Components/PersonalProjects';
+// import PersonalProjects from './Components/PersonalProjects';
 import Footer from './Components/Footer';
 import './styles/main.css'
 
@@ -14,7 +14,6 @@ class App extends Component {
         <Banner />
         <WhatWeDo />
         <RecentWork />
-        <PersonalProjects />
         <Testimonials />
         <Footer />
       </div>
