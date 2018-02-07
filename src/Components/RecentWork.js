a4233b9af5c50b9fa69b00d7e22d9e1f56edd987
Josh Holladay <josh@jcreateit.com>
2018-02-07 15:41:24 -0700

    Feat (assets) added two new projects to my website

--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import cryptoElevationHome from '../images/recent-work/crypto-elevation/crypto-elevation-home.jpg'
 import upssHome from '../images/recent-work/utah-plastic-surgery-society/utah-plastic-surgery-society-home.jpg'
+import strongBod from '../images/recent-work/StrongBod/strongBod.png'
+import unboxt from '../images/recent-work/Unboxt/unboxt.png'
 
 class Projects extends Component {
   render() {
@@ -46,6 +48,54 @@ class Projects extends Component {
             </div>
 
           </div>
+
+
+
+
+
+
+          <div className="divider"></div>
+
+
+          <div className="recent_work_services_container item-1 item-3">
+
+            <div className="recent_work_service crypto-img">
+              <img className="recent_work_img" src={strongBod} alt="StrongBod" />
+            </div>
+
+            <div className="recent_work_service crypto-content">
+              <div className="recent_work_services_content">
+                <p className="content-header align mobile-width">StrongBod</p>
+                <p className="tech-stack all-caps align mobile-width">Graphic Design, Photoshop, Illustrator, XD</p>
+                <p className="recent_work_main_content align mobile-width">StrongBod was a project I particularly enjoyed. Their goal was to present their product idea before actually building the app. I created a slide deck that far exceeded their expectations with nine custom mockups of mobile app screens, meaningful image use, and a design flow based consistant with presentational topics.</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="divider"></div>
+
+          <div className="recent_work_services_container item-2">
+
+            <div className="recent_work_service upss-img">
+              <img className="recent_work_img" src={unboxt} alt="Unboxt Logo" />
+            </div>
+
+            <div className="recent_work_service upss-content">
+              <div className="recent_work_services_content">
+                <p className="content-header align">MyCabinet / Unboxt</p>
+                <p className="tech-stack all-caps align">UX/UI Design, Consulting, Photoshop, Illustrator, XD</p>
+                <p className="recent_work_main_content align">MyCabinet is currently developing a new product called Unboxt. My role was to create mockups from wireframes. I considered how the user would expect each page and link to follow their thought process. I designed several pages including mobile views.</p>
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+
+
         </div>
       </div>
     );
