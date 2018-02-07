badc61f2d2a3978d3d65970d878292cb181fcb7d
Josh Holladay <josh@jcreateit.com>
2018-02-07 13:56:17 -0700

    Feat (style) banner font, image

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
+import ampersand from '../images/ampersand.png'
 
 class Header extends Component {
   render() {
     return (
       <div className="header-container">
-        <div className="title-one">
-          <p>Website Design</p>
-        </div>
+
         <div className="sub-title-one">
-          <p>that tells <span>your story</span></p>
+          <p>Website Design that tells <span>your story</span></p>
         </div>
-        <div className="title-two">
-          <p>Developed</p>
-        </div>
+
         <div className="sub-title-two">
-          <p>to change <span>their world</span></p>
+          <p>Developed to change <span>their world</span></p>
         </div>
       </div>
+
     );
   }
 }
@@ -25,15 +23,38 @@ export default Header;
 
 // <div className="header-container">
 //   <div className="title-one">
-//     <p>WEBSITE DESIGN</p>
+//     <p>Website Design</p>
 //   </div>
 //   <div className="sub-title-one">
 //     <p>that tells <span>your story</span></p>
 //   </div>
 //   <div className="title-two">
-//     <p>DEVELOPED</p>
+//     <p>Developed</p>
 //   </div>
 //   <div className="sub-title-two">
 //     <p>to change <span>their world</span></p>
 //   </div>
+// </div>
+
+
+// <div className="sub-title-one">
+//   <p>that tells <span>your story</span></p>
+// </div>
+// <div className="sub-title-two">
+//   <p>to change <span>their world</span></p>
+// </div>
+
+// <div className="header-container">
+//   <div className="title-one">
+//     <p>Design</p>
+//   </div>
+//
+//   <div className="ampersand">
+//     <p>&</p>
+//   </div>
+//
+//   <div className="title-two">
+//     <p>Development</p>
+//   </div>
+//
 // </div>
