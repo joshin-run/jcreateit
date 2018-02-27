e206071c6533266e3cd245e950e09acb3d771b2f
Josh Holladay <josh@jcreateit.com>
2018-02-26 17:50:40 -0700

    Feat (style) header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import ampersand from '../images/ampersand.png'
+// import downArrow from '../images/icons/down-circle-white.svg'
+
 
 class Header extends Component {
   render() {
     return (
       <div className="header-container">
+
         <div className="title-one">
-          <p>Website Design</p>
+          <p>Website Design that tells <span>your story</span>.</p>
         </div>
-        <div className="sub-title-one">
-          <p>that tells <span>your story</span></p>
-        </div>
+
         <div className="title-two">
-          <p>Developed</p>
+          <p>Developed, to change <span>their world</span>.</p>
         </div>
-        <div className="sub-title-two">
-          <p>to change <span>their world</span></p>
-        </div>
+
       </div>
     );
   }
 }
 
 export default Header;
+// <img className="down-arrow" src={downArrow} alt="Down Arrow" />
 
 // <div className="header-container">
 //
