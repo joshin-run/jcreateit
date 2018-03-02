16eb61e9d1762904baeca957582b63f4ad5c352f
Josh Holladay <josh@jcreateit.com>
2018-03-01 20:33:12 -0700

    Feat (style) projects card, uniform

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -23,10 +23,10 @@ class Portfolio extends Component {
             <img className="card-img" src={apio} alt="Card image cap" />
             <div className="card-container">
               <div className="card-content-wrapper">
-                <p className="card-header all-caps">Who they are</p>
-                <p className="card-content who-content">APiO is a Utah based FinTech company that leverages Big Data, Blockchain, AI and Machine Learning. They have transformed how capital is accessed and invested.</p>
-                <p className="card-header all-caps">What I did</p>
-                <p className="card-content">Created multiple mockups based on user stories and other provided graphics. <span>Stack: Adobe Photoshop, Illustrator, and Xd.</span></p>
+                {/*<p className="card-header all-caps">Who they are</p>*/}
+                <p className="card-content who-content">APiO is a Utah based FinTech company that leverages Big Data, Blockchain, AI and Machine Learning to transform how capital is accessed and invested.</p>
+                {/*<p className="card-header all-caps">What I did</p>*/}
+                <p className="card-content">I created multiple mockups based on user stories and other provided graphics. I created multiple mockups based on user stories and other provided graphics. I created multiple mockups based on user stories and other provided graphics. I created multiple mockups based on user <span>Stack: Adobe Photoshop, Illustrator, and XD. </span></p>
               </div>
             </div>
           </div>
@@ -37,13 +37,17 @@ class Portfolio extends Component {
             </div>
             <div className="card-container">
               <div className="card-content-wrapper">
-                <p className="card-header all-caps">Who they are</p>
+                {/*<p className="card-header all-caps">Who they are</p>*/}
                 <p className="card-content who-content">Unboxt is a company endeavoring to connect the job seeker to businesses all around the world.</p>
-                <p className="card-header all-caps">What I did</p>
-                <p className="card-content">Created mockup designs from wireframes and improved upon an already visually appealing website. <span>Stack: Photoshop, Illustrator, Xd.</span></p>
+                {/*<p className="card-header all-caps">What I did</p>*/}
+                <p className="card-content">I created mockup designs from wireframes representing dashboards and profile pages and improved upon their landing page. I created mockup designs from wireframes representing dashboards and profile pages and improved upon their landing page.I created mockup designs from wireframes representing <span>Stack: Photoshop, Illustrator, XD.</span></p>
               </div>
             </div>
           </div>
+
+
+
+
 
           <div className="card">
             <div id="unboxt">
@@ -51,10 +55,10 @@ class Portfolio extends Component {
             </div>
             <div className="card-container">
               <div className="card-content-wrapper">
-                <p className="card-header all-caps">Who they are</p>
+                {/*<p className="card-header all-caps">Who they are</p>*/}
                 <p className="card-content who-content">Crypto Elevation is a Global Cryptocurrency Opportunity that helps people all around the world create wealth and give back.</p>
-                <p className="card-header all-caps">What I did</p>
-                <p className="card-content what-content">Added several sections and pages to their site. Created uniformity in subpages with modern design. <span>Stack: HTML5, CSS3, Sass, JavaScript, Photoshop, Illustrator, and Xd.</span></p>
+                  {/*<p className="card-header all-caps">What I did</p>*/}
+                <p className="card-content what-content">I added several sections and pages to their site and created uniformity in with modern design. I added several <span>Stack: HTML5, CSS3, Sass, JavaScript, Photoshop, Illustrator, and XD.</span></p>
               </div>
 
               <div className="view-project-link-container">
@@ -73,3 +77,23 @@ class Portfolio extends Component {
 }
 
 export default Portfolio;
+
+// <div className="card">
+//   <div id="unboxt">
+//     <img className="card-img unboxt-img" src={crypto} alt="Card image cap" />
+//   </div>
+//   <div className="card-container">
+//     <div className="card-content-wrapper">
+//       {/*<p className="card-header all-caps">Who they are</p>*/}
+//       <p className="card-content who-content">Crypto Elevation is a Global Cryptocurrency Opportunity that helps people all around the world create wealth and give back.</p>
+//         {/*<p className="card-header all-caps">What I did</p>*/}
+//       <p className="card-content what-content">I added several sections and pages to their site and created uniformity in with modern design. <span>Stack: HTML5, CSS3, Sass, JavaScript, Photoshop, Illustrator, and XD.</span></p>
+//       <a href="https://www.behance.net/gallery/62567607/Crypto-Elevation">
+//       <div className="view-project-link-right-container">
+//         <p className="view-project-link-right">View Project ➔</p>
+//       </div>
+//       </a>
+//     </div>
+//
+//   </div>
+// </div>
