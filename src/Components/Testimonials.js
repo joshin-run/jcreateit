dec9b6ea5c48ee1991a4037e6969ce5afb42fa95
Josh Holladay <josh@jcreateit.com>
2018-05-11 10:39:29 -0600

    Feat (style) testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -16,22 +16,19 @@ class NavMenu extends Component {
 
           <div className="testimonials-content-container">
             <div className="testimonials-content">
-              <img className="testimonial-ce-img" src={ceLogo} alt="Crypto Elevation Logo" />
-              <p>"Josh Holladay at J Create It, LLC was a pleasure to work with. I appreciate all of his hard work and communication throughout the process.  Josh is always willing to take an idea, expand on the creation, and come out with a solid product.  He is very friendly and was a positive contributor to our project. Thank you Josh!"</p>
+              <p>"Josh Holladay at J Create It, LLC was a pleasure to work with. I appreciate all of his hard work and communication throughout the process.  Josh is always willing to take an idea, expand on the creation, and come out with a solid product.  He is very friendly and was a positive contributor to our project. Thank you Josh!"</p><br />
               <p>Kristina Walker</p>
               <p>Founder & Business Operations</p>
             </div>
 
             <div className="testimonials-content">
-              <img className="testimonial-dvi-img" src={bviLogo} alt="Burning Van Industries Logo" />
-              <p>"Josh is a very talented designer and was great to work with.  He was instrumental in adding his own artistic touch to enhance everything I suggested.  I will be sending a lot more design work his way soon! If he has bandwidth you should hire him before someone else does!"</p>
+              <p>"Josh is a very talented designer and was great to work with.  He was instrumental in adding his own artistic touch to enhance everything I suggested.  I will be sending a lot more design work his way soon! If he has bandwidth you should hire him before someone else does!"</p><br />
               <p>Jared Ables</p>
               <p>Full Stack Rails Developer</p>
             </div>
 
             <div className="testimonials-content">
-              <img className="testimonial-dvi-img" src={bviLogo} alt="Lowder Music Logo" />
-              <p>"I was very impressed with how many ideas and options Josh provided me with. I was able to pull from several different options to come up with one idea that I really liked. His response time is excellent. He takes your ideas and is adept at making them come alive. I couldn't be happier with the logo he designed for me as well as the business card. Graphic design at it's best."</p>
+              <p>"I was very impressed with how many ideas and options Josh provided me with. I was able to pull from several different options to come up with one idea that I really liked. His response time is excellent. He takes your ideas and is adept at making them come alive. I couldn't be happier with the logo he designed for me as well as the business card. Graphic design at it's best."</p><br />
               <p>Isaac Lowder</p>
               <p>Owner of Lowder Music</p>
             </div>
